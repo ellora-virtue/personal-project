8d0584ee407d91bf6af79950a20cdbf7cf172672
Ellora Virtue <ellora.virtue@hotmail.com>
2020-04-18 17:46:35 +1200

    Add logic to only display ingredients/measures that aren't null

--- client/components/Cocktails.jsx
+++ client/components/Cocktails.jsx
@@ -31,7 +31,7 @@ class Cocktails extends React.Component {
         // console.log(res.body)
         const x = res.body.drinks[0]
         const { strDrink, strInstructions, strDrinkThumb } = x
-        const ingredients = [[x.strIngredient1, x.strMeasure1], [x.strIngredient2, x.strMeasure2], [x.strIngredient3, x.strMeasure3], [x.strIngredient4, x.strMeasure4], [x.strIngredient5, x.strMeasure5]]
+        const ingredients = [[x.strIngredient1, x.strMeasure1], [x.strIngredient2, x.strMeasure2], [x.strIngredient3, x.strMeasure3], [x.strIngredient4, x.strMeasure4], [x.strIngredient5, x.strMeasure5], [x.strIngredient6, x.strMeasure6], [x.strIngredient7, x.strMeasure7], [x.strIngredient8, x.strMeasure8], [x.strIngredient9, x.strMeasure9], [x.strIngredient10, x.strMeasure10], [x.strIngredient11, x.strMeasure11], [x.strIngredient12, x.strMeasure12], [x.strIngredient13, x.strMeasure13], [x.strIngredient14, x.strMeasure14], [x.strIngredient15, x.strMeasure15]]
         // console.log(ingredients)
         this.setState({
           name: strDrink,
@@ -45,21 +45,21 @@ class Cocktails extends React.Component {
   render () {
     return (
       <>
-        <h1>Classic cocktails</h1>
-        <div><h2>{this.state.name}</h2></div>
+        <div><h1>{this.state.name}</h1></div>
         <div className='container'>
           <img src={this.state.image} style={imgStyle} alt={`image of a ${this.state.name}`} />
           <div className='innerCont'>
             <div className='ingredients'>
               <span id='title'>Ingredients: </span>
-              {console.log(this.state.ingredients)}
               <ul>
                 {this.state.ingredients.map((ingredient, idx) => {
-                  return (
-                    <li key={idx}>
-                      {ingredient[1]}  {ingredient[0]}
-                    </li>
-                  )
+                  if (ingredient[0] !== null && ingredient[1] !== null) {
+                    return (
+                      <li key={idx}>
+                        {ingredient[1]} {ingredient[0]}
+                      </li>
+                    )
+                  }
                 })}
               </ul>
             </div>
