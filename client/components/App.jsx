2c601854931c9e821fba82b2fcce0687cb32d94a
Ellora Virtue <ellora.virtue@hotmail.com>
2020-04-19 13:11:02 +1200

    Clean up code - remove comments etc.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 // import { Route } from 'react-router-dom'
 
 import Search from './Search'
-// import Cocktails from './Cocktails'
 
 const App = () => {
   return (
     <>
-      {/* <Cocktails /> */}
       <Search />
     </>
   )
--- client/components/Cocktails.jsx
+++ client/components/Cocktails.jsx
@@ -1,42 +1,13 @@
 import React from 'react'
-// import request from 'superagent'
-
-// Cocktail API end point
-// const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-// const cocktail = 'margarita'
-
-// strDrink, strIngredient1, strIngredient2, strIngredient3, strIngredient4,strInstructions, strDrinkThumb
 
 class Cocktails extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      // name: '',
-      // image: '',
-      // ingredients: [],
-      // instructions: '',
       drinks: props.drinks,
       cocktail: props.cocktail
     }
   }
-
-  // componentDidMount () {
-  //   request.get(apiUrl)
-  //     .query({ s: cocktail })
-  //     .then(res => {
-  //       // console.log(res.body)
-  //       const x = res.body.drinks[0]
-  //       const { strDrink, strInstructions, strDrinkThumb } = x
-  //       const ingredients = [[x.strIngredient1, x.strMeasure1], [x.strIngredient2, x.strMeasure2], [x.strIngredient3, x.strMeasure3], [x.strIngredient4, x.strMeasure4], [x.strIngredient5, x.strMeasure5], [x.strIngredient6, x.strMeasure6], [x.strIngredient7, x.strMeasure7], [x.strIngredient8, x.strMeasure8], [x.strIngredient9, x.strMeasure9], [x.strIngredient10, x.strMeasure10], [x.strIngredient11, x.strMeasure11], [x.strIngredient12, x.strMeasure12], [x.strIngredient13, x.strMeasure13], [x.strIngredient14, x.strMeasure14], [x.strIngredient15, x.strMeasure15]]
-  //       // console.log(ingredients)
-  //       this.setState({
-  //         name: strDrink,
-  //         image: strDrinkThumb,
-  //         instructions: strInstructions,
-  //         ingredients: ingredients
-  //       })
-  //     })
-  // }
 
   componentDidUpdate (prevProps) {
     if (prevProps.drinks !== this.props.drinks ||
@@ -49,7 +20,7 @@ class Cocktails extends React.Component {
     if (!this.state.drinks || !this.state.cocktail) {
       return null
     } else {
-      const drinks = this.state.drinks
+      // const drinks = this.state.drinks
       const x = this.state.cocktail
       const { strDrink: name, strInstructions: instructions, strDrinkThumb: image } = x
       const ingredients = [
