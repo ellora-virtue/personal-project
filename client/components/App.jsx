99a3534e30dea18edb08eda44020545ab43f392e
Ellora Virtue <ellora.virtue@hotmail.com>
2020-04-19 12:12:47 +1200

    Replace Home.jsx with Search.jsx to add search functionality - work in progress

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 // import { Route } from 'react-router-dom'
 
-// import Home from './Home'
-import Cocktails from './Cocktails'
+import Search from './Search'
+// import Cocktails from './Cocktails'
 
 const App = () => {
   return (
     <>
-      {/* <Home /> */}
-      <Cocktails />
-      {/* <Route path='/' component={Home} /> */}
-      {/* <Route exact path='/cocktail' component={Cocktails} /> */}
+      {/* <Cocktails /> */}
+      <Search />
     </>
   )
 }
---
+++ client/components/Search.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Cocktails from './Cocktails'
+
+const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+
+class Search extends React.Component { // 'React.'?
+  state = {
+    query: '',
+    drinks: [],
+    cocktail: null
+  }
+
+  updateQuery = event => {
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+  searchCocktails = () => {
+    fetch(`${apiUrl}${this.state.query}`)
+      .then(res => res.json())
+      .then(json => {
+        if (json.drinks.length > 0) {
+          const cocktail = json.drinks[0]
+          this.setState({ cocktail: cocktail, drinks: json.drinks })
+        }
+      })
+      .catch(error => console.log(error.message))
+  }
+
+  handleKeyPress = () => {
+    if (event.key === 'Enter') {
+      this.searchCocktails()
+    }
+  }
+
+  render () {
+    console.log(this.state.drinks)
+    return (
+      <>
+        <h1>What's your favourite cocktail?</h1>
+        <div className='search'>
+          <input className='input'
+            onKeyPress={this.handleKeyPress}
+            onChange={this.updateQuery}
+          />
+          <button className='button' onClick={this.searchCocktails}>
+            GO
+          </button>
+        </div>
+      </>
+    )
+  }
+}
+
+export default Search
