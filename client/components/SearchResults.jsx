0f2d9fad7f22727fde6d1f63b6efcb13059a71ca
Ellora Virtue <ellora.virtue@hotmail.com>
2020-06-05 13:41:00 +1200

    Page renders cocktail when clicked on
    - Not quite finished - bug: only renders first result no matter what one you click on?

--- client/components/SearchResults.jsx
+++ client/components/SearchResults.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Cocktails from './Cocktails'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 class SearchResults extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       drinks: props.drinks,
-      cocktail: null
+      cocktail: null,
+      // cocktailIdx: null,
+      renderRecipe: false
     }
   }
 
@@ -15,42 +17,51 @@ class SearchResults extends React.Component {
     window.location.reload(false)
   }
 
-  renderRecipe = () => {
-    // if (name === this.state.drinks.strDrink) {
-    console.log(name)
-    // }
+  handleClick = () => {
+    const selected = document.getElementById('selectedCocktail').value
+    // console.log(typeof this.state.drinks[0].idDrink)
+    const selectedCocktail = this.state.drinks.find(drink => Number(drink.idDrink) === selected)
+    this.setState({
+      drinks: this.state.drinks,
+      cocktail: selectedCocktail,
+      renderRecipe: true
+    })
   }
 
   render () {
-    return (
-      <div className='search-results'>
-        <h1 id='results-title'>Search results</h1>
-        
-        <ul>
-          {this.state.drinks.map((drink, idx) => {
-            const { strDrinkThumb: image, strDrink: name, strIngredient1, strIngredient2, strIngredient3 } = drink
-            return (
-              <li key={idx}>
-                <div className='result'>
-                  <img src={image} alt={`${name}`} />
-                  <div className='inner-result'>
-                    <Link to={`/cocktail/${name}`}>
-                      <h2 onClick={this.renderRecipe}>{name}</h2>
-                    </Link>
-                    <p>Ingredients: {strIngredient1}, {strIngredient2}, {strIngredient3}...</p>
+    console.log(this.state)
+    if (this.state.renderRecipe === true) {
+      return (
+        <Cocktails cocktail={this.state.cocktail} drinks={this.state.drinks} />
+      )
+    } else {
+      return (
+        <div className='search-results'>
+          <h1 id='results-title'>Search results</h1>
+          <ul>
+            {this.state.drinks.map((drink) => {
+              const { strDrinkThumb: image, strDrink: name, strIngredient1, strIngredient2, strIngredient3, idDrink: idx } = drink
+              return (
+                <li id='selectedCocktail' value={idx} key={idx}>
+                  <div className='result'>
+                    <img onClick={this.handleClick} src={image} alt={`${name}`} />
+                    <div className='inner-result'>
+                      <h2 onClick={this.handleClick}>{name}</h2>
+                      <p>Ingredients: {strIngredient1}, {strIngredient2}, {strIngredient3}...</p>
+                    </div>
                   </div>
-                </div>
-              </li>
-            )
-          })}
-        </ul>
-        <div className='cocktail'>
-          <button onClick={this.refreshPage}>
+                </li>
+              )
+            })}
+          </ul>
+          <div className='cocktail'>
+            <button onClick={this.refreshPage}>
             BACK TO SEARCH
-          </button>
+            </button>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
